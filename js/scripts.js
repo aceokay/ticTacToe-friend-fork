017412367c8b266c5ae7e24f2ee473572d7fbae3
Justin C Scott & Kyle Mellander <student@epicodus.com>
2015-09-16 09:25:47 -0700

    Add Space constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 function Player(mark) {
   this.mark = mark;
 };
+
+function Space(x, y) {
+  this.x = x;
+  this.y = y;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -2,5 +2,13 @@ describe("Player", function() {
   it("returns the player's symbol", function() {
     var testPlayer = new Player("tack");
     expect(testPlayer.mark).to.equal("tack");
-  })
-})
+  });
+});
+
+describe("Space", function() {
+  it("returns the coordinates of the space chosen", function() {
+    var testSpace = new Space(2, 2);
+    expect(testSpace.x).to.equal(2);
+    expect(testSpace.y).to.equal(2);
+  });
+});
