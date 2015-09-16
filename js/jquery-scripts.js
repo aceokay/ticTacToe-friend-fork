4a4790a44fab515aa85467f6598e5c82c3ff1424
Justin C Scott & Kyle Mellander <student@epicodus.com>
2015-09-16 15:11:38 -0700

    You can play a full game and get a victory point

--- js/jquery-scripts.js
+++ js/jquery-scripts.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $("#playerOneWeapon .choose").click(function() {
     var choice = $(this).attr('class').split(' ')[1];
-    players = [new Player(choice)];
-    turn = 0;
-    board = new Board(3,3);
+    game = new Game([new Player(choice)], 0, new Board(3,3));
 
-    $("#playerOneInfo").append("Player One <img class='info-pic' src='img/" + choice + ".jpg'> <span class='score'>0</span>");
+    $("#playerOneInfo").append("Player One <img class='info-pic' src='img/" + choice + ".jpg'> <span class='score'>" + game.players[0].score + "</span>");
 
     $("#playerOneWeapon").hide();
     $("#playerTwoWeapon").show();
@@ -14,19 +12,15 @@ $(document).ready(function() {
 
     $("#playerTwoWeapon .choose").click(function() {
       var choice = $(this).attr('class').split(' ')[1];
-      players.push(new Player(choice));
+      game.players.push(new Player(choice));
 
-      $("#playerTwoInfo").append("<span class='score'>0</span> <img class='info-pic' src='img/" + choice + ".jpg'> Player Two");
+      $("#playerTwoInfo").append("<span class='score'>" + game.players[1].score + "</span> <img class='info-pic' src='img/" + choice + ".jpg'> Player Two");
 
       $("#playerTwoWeapon").hide();
       $("#gameBoard").show();
 
-      $("#turnIndicator span").append('<img class="info-pic" src="img/' + players[turn].mark + '.jpg">')
+      $("#turnIndicator span").append('<img class="info-pic" src="img/' + game.players[game.turn].mark + '.jpg">')
 
     });
-
-
   });
-
-
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function Player(mark) {
   this.mark = mark;
+  this.score = 0;
 };
 
 function Space(x, y) {
@@ -7,28 +8,52 @@ function Space(x, y) {
   this.y = y;
 }
 
-Space.prototype.markBy = function(player) {
-  this.markBy = player;
-};
-
 function Board(x, y) {
   this.x = x;
   this.y = y;
   this.marks = [];
 }
 
+function Game(players, turn, board) {
+  this.players = players;
+  this.turn = turn;
+  this.board = board;
+}
+
+Space.prototype.markBy = function(player) {
+  this.markBy = player;
+};
+
 function choose(x, y, id) {
-  var newSpace = new Space(x, y);
-  newSpace.markBy(players[turn]);
-  board.mark(newSpace);
-  $("#gameBoard table td:eq("+ id +")").append('<img class="info-pic" src="img/' + players[turn].mark + '.jpg">')
-  if (turn === 0) {
-    turn = 1;
+  if ( $("#gameBoard table").hasClass('gameover') ) {
+    alert("your game is over!")
+  } else if (game.board.find(x, y) === undefined) {
+    $("#errors").hide();
+    var newSpace = new Space(x, y);
+    newSpace.markBy(game.players[game.turn]);
+    game.board.mark(newSpace);
+    $("#gameBoard table td:eq("+ id +")").append('<img class="info-pic" src="img/' + game.players[game.turn].mark + '.jpg">')
+    if (game.board.marks.length === 9) {
+      alert("Cats Game.  You both lose!");
+      $('#gameBoard table').addClass('gameover').on('click');
+    } else if (game.board.winner() === undefined) {
+      if (game.turn === 0) {
+        game.turn = 1;
+      } else {
+        game.turn = 0;
+      }
+      $("#turnIndicator span").empty();
+      $("#turnIndicator span").append('<img class="info-pic" src="img/' + game.players[game.turn].mark + '.jpg">')
+    } else {
+      $('#gameBoard table').addClass('gameover').on('click');
+      game.players[game.turn].score += 1;
+      $('#playerOneInfo .score').text(game.players[0].score);
+      $('#playerTwoInfo .score').text(game.players[1].score);
+    }
   } else {
-    turn = 0;
+    $("#errors").show();
+    $("#errors").text("Already Chosen! Choose an empty square");
   }
-  $("#turnIndicator span").empty();
-  $("#turnIndicator span").append('<img class="info-pic" src="img/' + players[turn].mark + '.jpg">')
 }
 
 Board.prototype.mark = function(space) {
@@ -58,27 +83,27 @@ Board.prototype.winner = function() {
   var threeThree = this.find(3,3);
 
   // Top Row Win
-  if (oneOne !== undefined && oneOne.markBy === oneTwo.markBy && oneTwo.markBy === oneThree.markBy) {
+  if (oneOne !== undefined && oneTwo !== undefined && oneThree !== undefined && oneOne.markBy === oneTwo.markBy && oneTwo.markBy === oneThree.markBy) {
     winner = oneOne.markBy;
   // Middle Row Win
-  } else if (twoOne !== undefined && twoOne.markBy === twoTwo.markBy && twoTwo.markBy === twoThree.markBy) {
+} else if (twoOne !== undefined && twoTwo !== undefined && twoThree !== undefined && twoOne.markBy === twoTwo.markBy && twoTwo.markBy === twoThree.markBy) {
     winner = twoOne.markBy;
   // Bottom Row Win
-  } else if (threeOne !== undefined && threeOne.markBy === threeTwo.markBy && threeTwo.markBy === threeThree.markBy) {
+} else if (threeOne !== undefined && threeTwo !== undefined && threeThree !== undefined && threeOne.markBy === threeTwo.markBy && threeTwo.markBy === threeThree.markBy) {
     winner = threeOne.markBy;
   // Left Column Win
-  } else if (oneOne !== undefined && oneOne.markBy === twoOne.markBy && twoOne.markBy === threeOne.markBy) {
+} else if (oneOne !== undefined && twoOne !== undefined && threeOne !== undefined && oneOne.markBy === twoOne.markBy && twoOne.markBy === threeOne.markBy) {
     winner = oneOne.markBy;
   // Middle Column Win
-  } else if (oneTwo !== undefined && oneTwo.markBy === twoTwo.markBy && twoTwo.markBy === threeTwo.markBy) {
+} else if (oneTwo !== undefined && twoTwo !== undefined && threeTwo !== undefined && oneTwo.markBy === twoTwo.markBy && twoTwo.markBy === threeTwo.markBy) {
     winner = oneTwo.markBy;
   // Right Column Win
-  } else if (oneThree !== undefined && oneThree.markBy === twoThree.markBy && twoThree.markBy === threeThree.markBy) {
+} else if (oneThree !== undefined && twoThree !== undefined && threeThree !== undefined && oneThree.markBy === twoThree.markBy && twoThree.markBy === threeThree.markBy) {
     winner = oneThree.markBy;
   // Diagonal Wins
-  } else if (oneOne !== undefined && oneOne.markBy === twoTwo.markBy && twoTwo.markBy === threeThree.markBy) {
+} else if (oneOne !== undefined && twoTwo !== undefined && threeThree !== undefined && oneOne.markBy === twoTwo.markBy && twoTwo.markBy === threeThree.markBy) {
     winner = oneOne.markBy;
-  } else if (oneThree !== undefined && oneThree.markBy === twoTwo.markBy && twoTwo.markBy === threeOne.markBy) {
+  } else if (oneThree !== undefined && twoTwo !== undefined && threeOne !== undefined && oneThree.markBy === twoTwo.markBy && twoTwo.markBy === threeOne.markBy) {
     winner = oneThree.markBy;
   }
 
